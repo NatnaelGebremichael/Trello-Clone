59845fdda553b69311fc7ab302918315671e69ca
Natnael Gebremichael <natnael92460@gmail.com>
2024-03-16 07:16:31 +1300

    TC-6 added pakages and crated sidbar.tsx to handle

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -1,5 +1,18 @@
 "use client";
+import Link from "next/link";
+import { Plus } from "lucide-react";
+import { useLocalStorage } from "usehooks-ts";
+import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 
-export const Sidebar = () => {
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Accordion } from "@/components/ui/accordion";
+
+interface SidebarProps {
+  storageKey?: string;
+}
+
+export const Sidebar = ({ storageKey }: SidebarProps) => {
   return <div>Side bar!</div>;
 };
