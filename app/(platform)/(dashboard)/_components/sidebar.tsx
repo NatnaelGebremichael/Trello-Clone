afab1ba741c7232447ff5c89f36f2d1213ceb62c
Natnael Gebremichael <natnael92460@gmail.com>
2024-03-20 20:23:58 +1300

    TC-6 Skeleton code that display when loading

--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -49,7 +49,15 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
   if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
     return (
       <>
-        <Skeleton />
+        <div className="flex items-center justify-between mb-2">
+          <Skeleton className="h-10 w-[50%]" />
+          <Skeleton className="h-10 w-10" />
+        </div>
+        <div className="space-y-2">
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+          <NavItem.Skeleton />
+        </div>
       </>
     );
   }
@@ -80,7 +88,7 @@ export const Sidebar = ({ storageKey = "t-sidebar-state" }: SidebarProps) => {
             isActive={activeOrganization?.id === organization.id}
             isExpanded={expanded[organization.id]}
             organization={organization as Organization}
-            onExpand = {onExpand}
+            onExpand={onExpand}
           />
         ))}
       </Accordion>
